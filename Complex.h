e3f56957565f9f8a917dc5ae6fc474469814361f
farhangd <44467574+farhangd@users.noreply.github.com>
2020-04-27 02:05:24 +0430

    add class Complex

---
+++ Complex.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 4/27/2020.
+//
+
+#ifndef GITTEST_COMPLEX_H
+#define GITTEST_COMPLEX_H
+
+
+class Complex {
+
+};
+
+
+#endif //GITTEST_COMPLEX_H
---
+++ cmake-build-debug/CMakeFiles/gitTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitTest.dir/Complex.cpp.o"
+  "CMakeFiles/gitTest.dir/main.cpp.o"
+  "gitTest.exe"
+  "gitTest.pdb"
+  "libgitTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
